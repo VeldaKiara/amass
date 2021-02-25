51e4fee8651f200acf743a374bc71ed16d75e068
veldakarimi <veldakarimi@gmail.com>
2021-02-25 16:11:47 +0300

    navbar addition

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,27 @@
+import "../assets/styles/Navbar.css";
+import logo from '../assets/logo.png';
+import conference from '../assets/conference.svg';
 const Nav = ()=>{
     return(
-        <h>Hello</h>
+      <div> 
+      <div className="logo"> 
+      <img src={logo} alt="logo" className="logoimg"></img>
+      </div>
+      <div className="signlog"> 
+      <a href=" "> Log in </a> &nbsp;&nbsp;
+      <a href=" "> Sign Up</a> 
+      </div>
+      <div className="ilustration"> 
+      <img src={conference} alt="conference" className="ill-img"></img>
+      </div> 
+      <br/>
+      <div className="belowl">
+      <h1> Discover tech events </h1> <br/>
+      </div>
+      <div className="btn">
+      <a href=" " className="btn-btn">Join Amass</a>
+      </div>
+      </div>
     )
 }
 
