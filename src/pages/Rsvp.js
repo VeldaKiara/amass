ada5463c66973673c3df822be4109fdb53c40966
veldakarimi <veldakarimi@gmail.com>
2021-04-05 16:44:03 +0300

    fix: rsvp input tel

--- src/pages/Rsvp.js
+++ src/pages/Rsvp.js
@@ -1,73 +1,87 @@
 import "../assets/styles/Rsvp.css";
+import mpesa from '../assets/images/mpesa.svg';
 const Rsvp = () =>{
     return(
-<div className='body-rsvp'> 
-<div class="mainContainer">
-  <div class="cardHolder">
-    <div class="header">
-      <div class="heading center">RSVP</div>
-      {/* <div class="stepHeading center">Payment Method</div> */}
-      <div class="card">
-        <div class="top part">
-          {/* <img src="https://i.imgrpost.com/imgr/2017/12/26/visa-1.png" alt="visa-1.png" border="0" /> */}
+<div className="mainContainer">
+  <div className="cardHolder">
+    <div className="header">
+      <div className="heading center">RSVP</div>
+      {/* <div className="stepHeading center">Payment Method</div> */}
+      <div className="card">
+        <div className="top part">
+          <img src={mpesa} alt="mpesa"></img>
         </div>
-        <div class="middle part">
-          <div class="infoheader vcenter">CARD NUMBER</div>
-          <div class="infocontent number vcenter">
-            <div class="num center">****</div>
-            <div class="num center">****</div>
-            <div class="num center">4658</div>
-            <div class="num center">****</div>
+        <div>
+        <form className="login-form">
+     <input type="text" placeholder="name"/>
+      <input type="text" placeholder="email address"/>
+      <input type="tel" placeholder="+254 712-345-678" pattern="+254 [0-9]{3}-[0-9]{3}-[0-9]{3}" required/>
+
+    </form>
+        </div>
+        {/* <div className="middle part">
+          <div className="infoheader vcenter">CARD NUMBER</div>
+          <div className="infocontent number vcenter">
+            <div className="num center">****</div>
+            <div className="num center">****</div>
+            <div className="num center">4658</div>
+            <div className="num center">****</div>
           </div>
         </div>
-        <div class="bottom part">
-          <div class="holderInfo">
-             <div class="infoheader vcenter">CARD HOLDER</div>
-            <div class="infocontent name vcenter">JOHN DOE</div>
+        <div className="bottom part">
+          <div className="holderInfo">
+             <div className="infoheader vcenter">CARD HOLDER</div>
+            <div className="infocontent name vcenter">JOHN DOE</div>
           </div>
-          <div class="expDate">
-            <div class="infoheader vcenter">EXP. DATE</div>
-            <div class="infocontent date vcenter">09/2023</div>
+          <div className="expDate">
+            <div className="infoheader vcenter">EXP. DATE</div>
+            <div className="infocontent date vcenter">09/2023</div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-  <div class="status vcenter"> <i class="fa fa-check" aria-hidden="true"></i>
-    Option : M-Pesa(mobile money)
-  </div>
-  
-  {/* <h5 class="center">OTHER PAYMENT OPTIONS</h5>
-  <div class="options vcenter">
-    <div class="opt">
-      <div class="icon center">
-        <i class="fa fa-money" aria-hidden="true"></i>
-      </div>
-      <div class="optname center">COD</div>
-    </div>
-     <div class="opt">
-      <div class="icon center">
-        <i class="fa fa-btc" aria-hidden="true"></i>
-      </div>
-      <div class="optname center">BitCoin</div>
-    </div>
-     <div class="opt">
-      <div class="icon center">
-        <i class="fa fa-google-wallet" aria-hidden="true"></i>
-      </div>
-      <div class="optname center">E-Wallet</div>
     </div>
   </div> */}
-  <div class="payment vcenter">
-    <div class="amount">
-      <div class="infoheader vcenter">Total Amount</div>
-      <div class="infocontent val vcenter">ksh 1</div>
+  <div className="status vcenter"> <i className="fa fa-check" aria-hidden="true"></i>
+    Option : M-Pesa(mobile money)
+  </div>
+  <div>
+
+ </div>
+  {/* <h5 className="center">OTHER PAYMENT OPTIONS</h5>
+  <div className="options vcenter">
+    <div className="opt">
+      <div className="icon center">
+        <i className="fa fa-money" aria-hidden="true"></i>
+      </div>
+      <div className="optname center">COD</div>
     </div>
-    <div class="button center">checkout</div>
+     <div className="opt">
+      <div className="icon center">
+        <i className="fa fa-btc" aria-hidden="true"></i>
+      </div>
+      <div className="optname center">BitCoin</div>
+    </div>
+     <div className="opt">
+      <div className="icon center">
+        <i className="fa fa-google-wallet" aria-hidden="true"></i>
+      </div>
+      <div className="optname center">E-Wallet</div>
+    </div>
+  </div> */}
+  <div className="payment vcenter">
+    <div className="amount">
+      <div className="infoheader vcenter">Total Amount</div>
+      <div className="infocontent val vcenter">ksh 1</div>
+    </div>
+    <div className="button center">checkout</div>
   </div>
 </div>
 
-    </div>
+</div>
+</div>
+</div>
+
+
     )
    
 }
